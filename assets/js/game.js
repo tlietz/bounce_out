2b72fad8fa020b4005608385523dfe6c9280bce9
tlietz <theodore.lietz@gmail.com>
2022-01-20 17:18:30 -0600

    put game into its own js file

---
+++ assets/js/game.js
@@ -0,0 +1,44 @@
+import Matter from "matter-js";
+
+export function startGame() {
+  // module aliases
+  var Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite;
+
+  // create an engine
+  var engine = Engine.create();
+
+  // create a renderer
+  var render = Render.create({
+    element: document.body,
+    engine: engine,
+  });
+
+  // create two boxes and a ground
+  var boxA = Bodies.rectangle(400, 200, 80, 80);
+  var boxB = Bodies.circle(450, 50, 20);
+  var ground = Bodies.rectangle(400, 610, 810, 60, {
+    isStatic: true,
+  });
+
+  // add all of the bodies to the world
+  Composite.add(engine.world, [boxA, boxB, ground]);
+
+  // Custom run function
+  (function run() {
+    window.requestAnimationFrame(run);
+    Engine.update(engine);
+  })();
+
+  // run the renderer
+  Render.run(render);
+
+  // create runner
+  var runner = Runner.create();
+
+  // run the engine
+  Runner.run(runner, engine);
+}
