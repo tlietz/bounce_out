31e49d8ec56ea31b91fb2bf4ef7eee58917005b6
tlietz <theodore.lietz@gmail.com>
2022-01-20 18:42:56 -0600

    seperate creation of bodies into its own function

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,16 +4,18 @@ const BORDER_W = 60;
 const SCREEN_W = 800;
 const SCREEN_H = 600;
 
-export function startGame() {
-    // module aliases
-    var Engine = Matter.Engine,
-        Render = Matter.Render,
-        Runner = Matter.Runner,
-        Composite = Matter.Composite,
-        MouseConstraint = Matter.MouseConstraint,
-        Mouse = Matter.Mouse,
-        Bodies = Matter.Bodies;
+const PIECE_R = 30;
 
+// module aliases
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Composite = Matter.Composite,
+    MouseConstraint = Matter.MouseConstraint,
+    Mouse = Matter.Mouse,
+    Bodies = Matter.Bodies;
+
+export function startGame() {
     // create an engine
     var engine = Engine.create();
     engine.gravity.y = 0;
@@ -23,14 +25,30 @@ export function startGame() {
         element: document.body,
         engine: engine,
         options: {
-            width: 800,
-            height: 600,
+            width: SCREEN_W,
+            height: SCREEN_H,
         },
     });
 
+    const bodies = createBodies();
+
+    // add all of the bodies to the world
+    Composite.add(engine.world, [...bodies]);
+
+    // run the renderer
+    Render.run(render);
+
+    // create runner
+    var runner = Runner.create();
+
+    // run the engine
+    Runner.run(runner, engine);
+}
+
+const createBodies = function () {
     const pieces = [
-        Bodies.circle(400, 200, 30),
-        Bodies.circle(450, 90, 30),
+        Bodies.circle(400, 200, PIECE_R),
+        Bodies.circle(450, 90, PIECE_R),
     ];
 
     const border = [
@@ -64,15 +82,5 @@ export function startGame() {
         ),
     ];
 
-    // add all of the bodies to the world
-    Composite.add(engine.world, [...pieces, ...border]);
-
-    // run the renderer
-    Render.run(render);
-
-    // create runner
-    var runner = Runner.create();
-
-    // run the engine
-    Runner.run(runner, engine);
-}
+    return [...pieces, ...border];
+};
