c23eb63cc575f59609d9d8dc90909459e036e603
tlietz <theodore.lietz@gmail.com>
2022-01-23 13:20:54 -0600

    adds launch strength multiplier and maximum launch velocity checks

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,6 +6,11 @@ const SCREEN_H = 600;
 
 const PIECE_R = 30;
 
+// determines the launch strength
+const LAUNCH_MULT = 0.07;
+
+const MAX_LAUNCH_VEL = 15;
+
 // The value of the collision category and collision mask of player's mouse.
 const P1 = 0x0002;
 
@@ -127,8 +132,15 @@ const createSensors = () => {
 // `end` is of the form {x, y}
 const launch = (piece, end) => {
     const start = piece.position;
-    const velX = end.x - start.x;
-    const velY = end.y - start.y;
+    let velX = (end.x - start.x) * LAUNCH_MULT;
+    if (velX > MAX_LAUNCH_VEL) {
+        velX = MAX_LAUNCH_VEL;
+    }
+    let velY = (end.y - start.y) * LAUNCH_MULT;
+    if (velY > MAX_LAUNCH_VEL) {
+        velY = MAX_LAUNCH_VEL;
+    }
+
     Body.setVelocity(piece, { x: velX, y: velY });
     console.log(`Launching with velocity: ${velX}, ${velY}`);
 };
