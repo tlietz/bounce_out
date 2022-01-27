1ffc5973fb497b573dbd515eca5ac7b833ab550f
tlietz <theodore.lietz@gmail.com>
2022-01-27 11:59:35 -0600

    setup initial server code

---
+++ test/bounce_out_web/channels/game_channel_test.exs
@@ -0,0 +1,27 @@
+defmodule BounceOutWeb.GameChannelTest do
+  use BounceOutWeb.ChannelCase
+
+  setup do
+    {:ok, _, socket} =
+      BounceOutWeb.UserSocket
+      |> socket("user_id", %{some: :assign})
+      |> subscribe_and_join(BounceOutWeb.GameChannel, "game:lobby")
+
+    %{socket: socket}
+  end
+
+  test "ping replies with status ok", %{socket: socket} do
+    ref = push(socket, "ping", %{"hello" => "there"})
+    assert_reply ref, :ok, %{"hello" => "there"}
+  end
+
+  test "shout broadcasts to game:lobby", %{socket: socket} do
+    push(socket, "shout", %{"hello" => "all"})
+    assert_broadcast "shout", %{"hello" => "all"}
+  end
+
+  test "broadcasts are pushed to the client", %{socket: socket} do
+    broadcast_from!(socket, "broadcast", %{"some" => "data"})
+    assert_push "broadcast", %{"some" => "data"}
+  end
+end
